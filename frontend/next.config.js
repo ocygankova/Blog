0abb03f9304630958219daafa1e70d48ab6f3b59
Olga Cygankowa <olga.cygankowa@gmail.com>
2023-06-01 16:12:59 +0200

    Install frontend libraries, add bundle-analyzer setup

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["localhost"],
+    deviceSizes: [576, 768, 992, 1200, 1400],
+  },
+};
 
-module.exports = nextConfig
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+// module.exports = nextConfig;
+
+module.exports = withBundleAnalyzer(nextConfig);
