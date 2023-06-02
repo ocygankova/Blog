7f1a3fe03b945367440293d56a4c99c0746a3a61
Olga Cygankowa <olga.cygankowa@gmail.com>
2023-06-02 18:49:28 +0200

    Add axios setup and api calls

---
+++ frontend/src/http/api/blog.ts
@@ -0,0 +1,66 @@
+import api from "@/http/axiosInstance";
+import { IBlogPost, IBlogPostsPage } from "@/models/blogPost";
+
+export async function getBlogPosts(page: number = 1) {
+  const res = await api.get<IBlogPostsPage>(`/posts?page=${page}`);
+  return res.data;
+}
+
+export async function getBlogPostsByUser(userId: string, page: number = 1) {
+  const res = await api.get<IBlogPostsPage>(
+    `/posts?authorId=${userId}&page=${page}`
+  );
+  return res.data;
+}
+
+export async function getAllBlogPostSlugs() {
+  const res = await api.get<string[]>("/posts/slugs");
+  return res.data;
+}
+
+export async function getBlogPostBySlug(slug: string) {
+  const res = await api.get<IBlogPost>(`/posts/post/${slug}`);
+  return res.data;
+}
+
+interface ICreateBlogPostValues {
+  slug: string;
+  title: string;
+  summary: string;
+  body: string;
+  postImage: File;
+}
+
+export async function createBlogPost(input: ICreateBlogPostValues) {
+  const formData = new FormData(); //using FormData instead of json to send file
+  Object.entries(input).forEach(([key, value]) => {
+    formData.append(key, value);
+  });
+
+  const res = await api.post<IBlogPost>("/posts", formData);
+  return res.data;
+}
+
+interface IUpdateBlogPostValues {
+  slug: string;
+  title: string;
+  summary: string;
+  body: string;
+  postImage?: File;
+}
+
+export async function updateBlogPost(
+  blogPostId: string,
+  input: IUpdateBlogPostValues
+) {
+  const formData = new FormData();
+  Object.entries(input).forEach(([key, value]) => {
+    if (value !== undefined) formData.append(key, value);
+  });
+
+  await api.patch(`/posts/${blogPostId}`, formData);
+}
+
+export async function deleteBlogPost(blogPostId: string) {
+  await api.delete(`/posts/${blogPostId}`);
+}
---
+++ frontend/src/http/api/users.ts
@@ -0,0 +1,74 @@
+import api from "@/http/axiosInstance";
+import { IUser } from "@/models/user";
+
+export async function getAuthenticatedUser() {
+  const res = await api.get<IUser>("/users/me");
+  return res.data;
+}
+
+export async function getUserByUsername(username: string) {
+  const res = await api.get<IUser>(`/users/profile/${username}`);
+  return res.data;
+}
+
+interface ISignUpValues {
+  username: string;
+  email: string;
+  password: string;
+  verificationCode: string;
+}
+
+export async function signUp(credentials: ISignUpValues) {
+  const res = await api.post<IUser>("/users/signup", credentials);
+  return res.data;
+}
+
+export async function requestEmailVerificationCode(email: string) {
+  await api.post("/users/verification-code", { email });
+}
+
+export async function requestPasswordResetCode(email: string) {
+  await api.post("/users/reset-password-code", { email });
+}
+
+interface IResetPasswordValues {
+  email: string;
+  password: string;
+  verificationCode: string;
+}
+
+export async function resetPassword(credentials: IResetPasswordValues) {
+  const res = await api.post<IUser>("/users/reset-password", credentials);
+  return res.data;
+}
+
+interface ILoginValues {
+  username: string;
+  password: string;
+}
+
+export async function logIn(credentials: ILoginValues) {
+  const res = await api.post<IUser>("/users/login", credentials);
+  return res.data;
+}
+
+export async function logOut() {
+  await api.post("/users/logout");
+}
+
+interface IUpdateUserValues {
+  username?: string;
+  displayName?: string;
+  about?: string;
+  profileImage?: File;
+}
+
+export async function updateUser(input: IUpdateUserValues) {
+  const formData = new FormData();
+  Object.entries(input).forEach(([key, value]) => {
+    if (value !== undefined) formData.append(key, value);
+  });
+
+  const res = await api.patch<IUser>("/users/me", formData);
+  return res.data;
+}
