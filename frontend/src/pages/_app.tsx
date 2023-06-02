b6cf09c251051f0c537b5ecd71a8bbe1d73d0023
Olga Cygankowa <olga.cygankowa@gmail.com>
2023-06-02 18:35:08 +0200

    Add footer, error pages, remove placeholder data from homepage

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,8 +3,11 @@ import { AppProps } from "next/app";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { CacheProvider, EmotionCache } from "@emotion/react";
+import NextNProgress from "nextjs-progressbar";
 import basicTheme from "@/styles/theme";
 import createEmotionCache from "@/styles/createEmotionCache";
+import { Footer } from "@/components";
+import { Container } from "@mui/material";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -18,11 +21,26 @@ export default function MyApp(props: MyAppProps) {
   return (
     <CacheProvider value={emotionCache}>
       <Head>
+        <title>Blog - Share your ideas</title>
+        <meta name="description" content="A blog to share your ideas" />
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={basicTheme}>
         <CssBaseline />
-        <Component {...pageProps} />
+        <NextNProgress
+          color="#21fa90"
+          height={5}
+          options={{
+            showSpinner: false,
+          }}
+        />
+
+        <main>
+          <Container>
+            <Component {...pageProps} />
+          </Container>
+        </main>
+        <Footer />
       </ThemeProvider>
     </CacheProvider>
   );
