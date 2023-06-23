078fd5db9716d45e1bf71bedadd94bc26e444bb0
Olga Cygankowa <olga.cygankowa@gmail.com>
2023-06-23 15:56:29 +0200

    Add onboarding page and redirect

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,13 +1,14 @@
-import Head from "next/head";
-import { AppProps } from "next/app";
-import { ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import { CacheProvider, EmotionCache } from "@emotion/react";
-import NextNProgress from "nextjs-progressbar";
-import basicTheme from "@/styles/theme";
-import createEmotionCache from "@/styles/createEmotionCache";
-import { Footer, Header } from "@/components";
-import { Container } from "@mui/material";
+import Head from 'next/head';
+import { AppProps } from 'next/app';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import NextNProgress from 'nextjs-progressbar';
+import basicTheme from '@/styles/theme';
+import createEmotionCache from '@/styles/createEmotionCache';
+import { Footer, Header } from '@/components';
+import { Container } from '@mui/material';
+import { useOnboardingRedirect } from '@/hooks';
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -17,7 +18,10 @@ export interface MyAppProps extends AppProps {
 }
 
 export default function MyApp(props: MyAppProps) {
+  useOnboardingRedirect();
+
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
   return (
     <CacheProvider value={emotionCache}>
       <Head>
---
+++ frontend/src/pages/onboarding/index.tsx
@@ -0,0 +1,102 @@
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import * as yup from 'yup';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Alert, Box, Stack, Typography } from '@mui/material';
+import { maxLengths, usernameSchema } from '@/utils';
+import { useAuthenticatedUser } from '@/hooks';
+import { BadRequestError, ConflictError } from '@/http/http-errors';
+import * as UsersApi from '@/http/api/users';
+import { AuthFormInputField, LoadingButton } from '@/components';
+
+const validationSchema = yup.object({
+  username: usernameSchema.required('Username is required.'),
+});
+
+type IOnboardingInput = yup.InferType<typeof validationSchema>;
+
+function OnboardingPage() {
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  const router = useRouter();
+  const { user, userLoading, mutateUser } = useAuthenticatedUser();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { isSubmitting, errors },
+  } = useForm<IOnboardingInput>({
+    resolver: yupResolver(validationSchema),
+  });
+
+  const onSubmit = async ({ username }: IOnboardingInput) => {
+    try {
+      setErrorMessage(null);
+      const updatedUser = await UsersApi.updateUser({
+        username,
+        displayName: username,
+      });
+      mutateUser(updatedUser);
+    } catch (error) {
+      if (error instanceof ConflictError || error instanceof BadRequestError) {
+        setErrorMessage(error.message);
+      } else {
+        console.log(error);
+        alert(error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (!userLoading && !user) {
+      router.push('/');
+    }
+    if (user?.username) {
+      const returnTo = router.query.returnTo?.toString();
+      router.push(returnTo || '/');
+    }
+  }, [user, router]);
+
+  return (
+    <>
+      <Typography variant="h1" mb={4}>
+        Onboarding
+      </Typography>
+
+      <Typography variant="h4" component="p" mb={4} lineHeight="200%">
+        Thank you for signing up!
+        <br />
+        Before we continue, please set your username!
+      </Typography>
+
+      <Box maxWidth={800}>
+        {errorMessage && (
+          <Alert severity="error" sx={{ mb: 4 }}>
+            {errorMessage}
+          </Alert>
+        )}
+
+        <Stack component="form" noValidate onSubmit={handleSubmit(onSubmit)} spacing={2}>
+          <AuthFormInputField
+            register={register('username')}
+            label="Username"
+            validationError={errors.username}
+            watch={watch}
+            maxLength={maxLengths.userName}
+          />
+
+          <LoadingButton
+            type="submit"
+            isLoading={isSubmitting}
+            variant="contained"
+            sx={{ alignSelf: { sm: 'flex-end' }, px: 10 }}>
+            Submit
+          </LoadingButton>
+        </Stack>
+      </Box>
+    </>
+  );
+}
+
+export default OnboardingPage;
